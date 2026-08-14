9a1a380aef715ffc6e4f90f9e0d68857a0deb38d
Farrel Satriano <Farrelsatriano2@gmail.com>
2026-08-15 00:50:08 +0700

    Update import path for database module

--- mainfile/models.py
+++ mainfile/models.py
@@ -2,7 +2,7 @@ from datetime import date
 from decimal import Decimal
 from sqlalchemy import Boolean, Date, Numeric, CheckConstraint, String, ForeignKey, Text
 from sqlalchemy.orm import Mapped, relationship, mapped_column
-from app.database import Base
+from mainfile.database import Base
 
 class Specialty(Base):
   __tablename__ = "specialties"
