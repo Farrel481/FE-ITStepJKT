ca6c4289b2406099420ec6f4024a80e6a477fcb7
Farrel Satriano <Farrelsatriano2@gmail.com>
2026-08-15 00:23:35 +0700

    Add initial imports to models.py

---
+++ mainfile/models.py
@@ -0,0 +1,6 @@
+from datetime import date
+from decimal import Decimal
+from sqlalchemy import Boolean, Date, Numeric, CheckConstraint, String, ForeignKey, Text
+from sqlalchemy import Mapped, relationship, mapped_column
+from app.database import Base
+
