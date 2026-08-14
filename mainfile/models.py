536036bef208070d5f7db7e60a22851ff9802f14
Farrel Satriano <Farrelsatriano2@gmail.com>
2026-08-15 00:34:50 +0700

    Add models for Specialty, Doctor, Patient, Appointment, and Prescription

--- mainfile/models.py
+++ mainfile/models.py
@@ -4,3 +4,17 @@ from sqlalchemy import Boolean, Date, Numeric, CheckConstraint, String, ForeignK
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 from app.database import Base
 
+class Specialty(Base):
+  __tablename__ = "specialties"
+
+class Doctor(Base):
+  __tablename__ = "doctors"
+
+class Patient(Base):
+  __tablename__ = "patients"
+
+class Appointment(Base):
+  __tablename__ = "appointments"
+
+class Prescription(Base):
+  __tablename__ = "prescriptions"
